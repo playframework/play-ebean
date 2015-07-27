6d6b7b7f186f3004332757a714820ddf9f8674e9
Mirco Dotta <mirco.dotta@gmail.com>
2015-07-27 10:39:49 +0200

    Project compiles against Play master. Fix #49
    Because `Action.call` does no longer declare to throw in its signature, there is
    no need to wrap `Throwable` into a runtime exception.
    
    This commit is related to https://github.com/playframework/playframework/commit/f9e2d1917019f2bb1b2876b464014640965bade1#diff-02a5773c77e32371d3322bbb15c320d7L27 (see playframework/playframework@f9e2d1917019f2bb1b2876b464014640965bade1)

--- play-ebean/src/main/java/play/db/ebean/TransactionalAction.java
+++ play-ebean/src/main/java/play/db/ebean/TransactionalAction.java
@@ -3,9 +3,10 @@
  */
 package play.db.ebean;
 
+import java.util.concurrent.CompletionStage;
+
 import com.avaje.ebean.Ebean;
 import com.avaje.ebean.TxCallable;
-import play.libs.F;
 import play.mvc.Action;
 import play.mvc.Http.Context;
 import play.mvc.Result;
@@ -14,19 +15,7 @@ import play.mvc.Result;
  * Wraps an action in an Ebean transaction.
  */
 public class TransactionalAction extends Action<Transactional> {
-    
-    public F.Promise<Result> call(final Context ctx) throws Throwable {
-        return Ebean.execute(new TxCallable<F.Promise<Result>>() {
-            public F.Promise<Result> call() {
-                try {
-                    return delegate.call(ctx);
-                } catch (RuntimeException e) {
-                    throw e;
-                } catch (Throwable t) {
-                    throw new RuntimeException(t);
-                }
-            }
-        });
+    public CompletionStage<Result> call(final Context ctx) {
+        return Ebean.execute(() -> delegate.call(ctx));
     }
-    
 }
