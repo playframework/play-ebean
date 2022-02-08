d22572161f4e2131bd11d364a1da882a5523ac39
Scala Steward <me@scala-steward.org>
2022-02-08 14:51:34 +0100

    Reformat with scalafmt 2.7.5

--- sbt-play-ebean/src/main/scala-sbt-0.13/play/ebean/sbt/PlayEbean.scala
+++ sbt-play-ebean/src/main/scala-sbt-0.13/play/ebean/sbt/PlayEbean.scala
@@ -14,8 +14,11 @@ object PlayEbean extends AutoPlugin {
 
   object autoImport {
     val playEbeanModels = taskKey[Seq[String]]("The packages that should be searched for ebean models to enhance.")
-    val playEbeanVersion = settingKey[String]("The version of Play ebean that should be added to the library dependencies.")
-    val playEbeanDebugLevel = settingKey[Int]("The debug level to use for the ebean agent. The higher, the more debug is output, with 9 being the most. -1 turns debugging off.")
+    val playEbeanVersion =
+      settingKey[String]("The version of Play ebean that should be added to the library dependencies.")
+    val playEbeanDebugLevel = settingKey[Int](
+      "The debug level to use for the ebean agent. The higher, the more debug is output, with 9 being the most. -1 turns debugging off."
+    )
     val playEbeanAgentArgs = taskKey[Map[String, String]]("The arguments to pass to the agent.")
   }
 
@@ -34,19 +37,19 @@ object PlayEbean extends AutoPlugin {
     playEbeanDebugLevel := -1,
     playEbeanAgentArgs := Map("debug" -> playEbeanDebugLevel.value.toString),
     playEbeanVersion := readResourceProperty("play-ebean.version.properties", "play-ebean.version"),
-    libraryDependencies += "com.typesafe.play" %% "play-ebean" % playEbeanVersion.value,
+    libraryDependencies += "com.typesafe.play" %% "play-ebean"   % playEbeanVersion.value,
     libraryDependencies += "org.glassfish.jaxb" % "jaxb-runtime" % "2.3.2"
   )
 
   def ebeanEnhance: Def.Initialize[Task[Compiler.CompileResult]] = Def.task {
 
-    val deps = dependencyClasspath.value
-    val classes = classDirectory.value
-    val result = manipulateBytecode.value
+    val deps      = dependencyClasspath.value
+    val classes   = classDirectory.value
+    val result    = manipulateBytecode.value
     val agentArgs = playEbeanAgentArgs.value
-    val analysis = result.analysis
+    val analysis  = result.analysis
 
-    val agentArgsString = agentArgs map { case (key, value) => s"$key=$value" } mkString ";"
+    val agentArgsString = agentArgs.map { case (key, value) => s"$key=$value" }.mkString(";")
 
     val originalContextClassLoader = Thread.currentThread.getContextClassLoader
 
@@ -83,9 +86,9 @@ object PlayEbean extends AutoPlugin {
      * be supported.
      */
     def updateStampForClassFile(classFile: File, stamp: Stamp): Stamp = stamp match {
-      case _: Exists => Stamp.exists(classFile)
+      case _: Exists       => Stamp.exists(classFile)
       case _: LastModified => Stamp.lastModified(classFile)
-      case _: Hash => Stamp.hash(classFile)
+      case _: Hash         => Stamp.hash(classFile)
     }
     // Since we may have modified some of the products of the incremental compiler, that is, the compiled template
     // classes and compiled Java sources, we need to update their timestamps in the incremental compiler, otherwise
@@ -93,8 +96,10 @@ object PlayEbean extends AutoPlugin {
     val updatedAnalysis = analysis.copy(stamps = allProducts.foldLeft(analysis.stamps) { (stamps, classFile) =>
       val existingStamp = stamps.product(classFile)
       if (existingStamp == Stamp.notPresent) {
-        throw new java.io.IOException("Tried to update a stamp for class file that is not recorded as "
-          + s"product of incremental compiler: $classFile")
+        throw new java.io.IOException(
+          "Tried to update a stamp for class file that is not recorded as "
+            + s"product of incremental compiler: $classFile"
+        )
       }
       stamps.markProduct(classFile, updateStampForClassFile(classFile, existingStamp))
     })
@@ -102,15 +107,16 @@ object PlayEbean extends AutoPlugin {
     result.copy(analysis = updatedAnalysis)
   }
 
-
   private def configuredEbeanModels = Def.task {
     import collection.JavaConverters._
-    import java.util.{ Map => JMap, List => JList }
+    import java.util.{ Map => JMap }
+    import java.util.{ List => JList }
 
     // Creates a classloader with all the dependencies and all the resources, from there we can use the play ebean
     // code to load the config as it would be loaded in production
     def withClassLoader[T](block: ClassLoader => T): T = {
-      val classpath = unmanagedResourceDirectories.value.map(_.toURI.toURL) ++ dependencyClasspath.value.map(_.data.toURI.toURL)
+      val classpath =
+        unmanagedResourceDirectories.value.map(_.toURI.toURL) ++ dependencyClasspath.value.map(_.data.toURI.toURL)
       val classLoader = new URLClassLoader(classpath.toArray, null)
       try {
         block(classLoader)
@@ -134,8 +140,9 @@ object PlayEbean extends AutoPlugin {
     }
 
     withClassLoader { classLoader =>
-      val configLoader = classLoader.loadClass("play.db.ebean.ModelsConfigLoader").
-        asSubclass(classOf[java.util.function.Function[ClassLoader, JMap[String, JList[String]]]])
+      val configLoader = classLoader
+        .loadClass("play.db.ebean.ModelsConfigLoader")
+        .asSubclass(classOf[java.util.function.Function[ClassLoader, JMap[String, JList[String]]]])
       val config = configLoader.newInstance().apply(classLoader)
 
       if (config.isEmpty) {
@@ -147,7 +154,7 @@ object PlayEbean extends AutoPlugin {
   }
 
   private def readResourceProperty(resource: String, property: String): String = {
-    val props = new java.util.Properties
+    val props  = new java.util.Properties
     val stream = getClass.getClassLoader.getResourceAsStream(resource)
     try { props.load(stream) }
     catch { case e: Exception => }
