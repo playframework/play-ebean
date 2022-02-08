23283f0701207801a63760bd171be7245a4cf951
Scala Steward <me@scala-steward.org>
2022-02-08 14:50:33 +0100

    Update ebean, ebean-ddl-generator to 12.8.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,34 +9,34 @@ object Dependencies {
   }
 
   object Versions {
-    val play: String = "2.8.0"
-    val ebean = "12.8.1"
-    val ebeanAgent = "12.8.1"
+    val play: String   = "2.8.0"
+    val ebean          = "12.8.3"
+    val ebeanAgent     = "12.8.3"
     val typesafeConfig = "1.3.4"
   }
 
   val ebean = libraryDependencies ++= Seq(
-    "io.ebean" % "ebean" % Versions.ebean,
-    "io.ebean" % "ebean-ddl-generator" % Versions.ebean,
-    "io.ebean" % "ebean-agent" % Versions.ebeanAgent,
-    "com.typesafe.play" %% "play-java-jdbc" % Versions.play,
+    "io.ebean"           % "ebean"                % Versions.ebean,
+    "io.ebean"           % "ebean-ddl-generator"  % Versions.ebean,
+    "io.ebean"           % "ebean-agent"          % Versions.ebeanAgent,
+    "com.typesafe.play" %% "play-java-jdbc"       % Versions.play,
     "com.typesafe.play" %% "play-jdbc-evolutions" % Versions.play,
-    "com.typesafe.play" %% "play-guice" % Versions.play % Test,
-    "com.typesafe.play" %% "filters-helpers" % Versions.play % Test,
-    "com.typesafe.play" %% "play-test" % Versions.play % Test,
-    ("org.reflections" % "reflections" % "0.9.12")
+    "com.typesafe.play" %% "play-guice"           % Versions.play % Test,
+    "com.typesafe.play" %% "filters-helpers"      % Versions.play % Test,
+    "com.typesafe.play" %% "play-test"            % Versions.play % Test,
+    ("org.reflections"   % "reflections"          % "0.9.12")
       .exclude("com.google.code.findbugs", "annotations")
       .classifier("")
   )
 
   val plugin = libraryDependencies ++= Seq(
-    "io.ebean" % "ebean" % Versions.ebean,
-    "io.ebean" % "ebean-agent" % Versions.ebeanAgent,
-    "com.typesafe" % "config" % Versions.typesafeConfig,
-    "com.typesafe.play" %% "play-java-jdbc" % Versions.play,
+    "io.ebean"           % "ebean"                % Versions.ebean,
+    "io.ebean"           % "ebean-agent"          % Versions.ebeanAgent,
+    "com.typesafe"       % "config"               % Versions.typesafeConfig,
+    "com.typesafe.play" %% "play-java-jdbc"       % Versions.play,
     "com.typesafe.play" %% "play-jdbc-evolutions" % Versions.play,
-    "com.typesafe.play" %% "play-guice" % Versions.play % Test,
-    "com.typesafe.play" %% "filters-helpers" % Versions.play % Test,
-    "com.typesafe.play" %% "play-test" % Versions.play % Test
+    "com.typesafe.play" %% "play-guice"           % Versions.play % Test,
+    "com.typesafe.play" %% "filters-helpers"      % Versions.play % Test,
+    "com.typesafe.play" %% "play-test"            % Versions.play % Test
   )
 }
