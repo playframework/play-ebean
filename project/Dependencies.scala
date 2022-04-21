da2ff24b691eb35463f77a1626def65feba37be6
Scala Steward <me@scala-steward.org>
2022-04-21 15:20:00 +0200

    Update ebean, ebean-agent, ... to 13.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
 
   object Versions {
     val play: String   = "2.8.15"
-    val ebean          = "12.16.1"
+    val ebean          = "13.4.0"
     val typesafeConfig = "1.4.2"
   }
 
