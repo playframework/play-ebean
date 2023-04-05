3808270324abe1ae96be1d51beceef23f8992c4c
Matthias Kurz <m.kurz@irregular.at>
2023-04-05 11:15:01 +0200

    No need for CrossVersion.for3Use2_13 anymore for com.typesafe artifacts

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,6 @@
 import sbt.Keys.libraryDependencies
 
 import sbt._
-import scala.util.Properties
 
 object Dependencies {
 
@@ -12,41 +11,25 @@ object Dependencies {
   }
 
   object Versions {
-    val play: String   = "2.9.0-M3-SNAPSHOT"
+    val play: String   = "2.9.0-M3"
     val ebean          = "13.15.2"
     val typesafeConfig = "1.4.2"
   }
 
-  val ebean = libraryDependencies ++= {
-    CrossVersion.partialVersion(Properties.versionNumberString) match {
-        case Some((3, n)) => Seq(
-          "io.ebean"           % "ebean"                % Versions.ebean,
-          "io.ebean"           % "ebean-ddl-generator"  % Versions.ebean,
-          "io.ebean"           % "ebean-agent"          % Versions.ebean,
-          ("com.typesafe.play" %% "play-java-jdbc"       % Versions.play).cross(CrossVersion.for3Use2_13),
-          ("com.typesafe.play" %% "play-jdbc-evolutions" % Versions.play).cross(CrossVersion.for3Use2_13),
-          "com.typesafe.play" %% "play-guice"           % Versions.play % Test,
-          "com.typesafe.play" %% "filters-helpers"      % Versions.play % Test,
-          "com.typesafe.play" %% "play-test"            % Versions.play % Test,
-          ("org.reflections"   % "reflections"          % "0.10.2")
-            .exclude("com.google.code.findbugs", "annotations")
-            .classifier("")
-        )
-        case _          => Seq(
-          "io.ebean"           % "ebean"                % Versions.ebean,
-          "io.ebean"           % "ebean-ddl-generator"  % Versions.ebean,
-          "io.ebean"           % "ebean-agent"          % Versions.ebean,
-          ("com.typesafe.play" %% "play-java-jdbc"       % Versions.play),
-          ("com.typesafe.play" %% "play-jdbc-evolutions" % Versions.play),
-          ("com.typesafe.play" %% "play-guice"           % Versions.play % Test),
-          ("com.typesafe.play" %% "filters-helpers"      % Versions.play % Test),
-          ("com.typesafe.play" %% "play-test"            % Versions.play % Test),
-          ("org.reflections"   % "reflections"          % "0.10.2")
-            .exclude("com.google.code.findbugs", "annotations")
-            .classifier("")
-        )
-    }
-  }
+  val ebean = libraryDependencies ++= Seq(
+    "io.ebean"           % "ebean"                % Versions.ebean,
+    "io.ebean"           % "ebean-ddl-generator"  % Versions.ebean,
+    "io.ebean"           % "ebean-agent"          % Versions.ebean,
+    "com.typesafe.play" %% "play-java-jdbc"       % Versions.play,
+    "com.typesafe.play" %% "play-jdbc-evolutions" % Versions.play,
+    "com.typesafe.play" %% "play-guice"           % Versions.play % Test,
+    "com.typesafe.play" %% "filters-helpers"      % Versions.play % Test,
+    "com.typesafe.play" %% "play-test"            % Versions.play % Test,
+    ("org.reflections"   % "reflections"          % "0.10.2")
+      .exclude("com.google.code.findbugs", "annotations")
+      .classifier("")
+  )
+
   val plugin = libraryDependencies ++= Seq(
     "io.ebean"     % "ebean"       % Versions.ebean,
     "io.ebean"     % "ebean-agent" % Versions.ebean,
