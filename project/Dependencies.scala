e3d3018d53b6d073b4268a1fc4f85de161be9363
Scala Steward <scala_steward@virtuslab.com>
2023-04-21 18:28:31 +0000

    scala3-library 3.3.0-RC4 (was 3.3.0-RC3)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   object ScalaVersions {
     val scala212 = "2.12.17"
     val scala213 = "2.13.10"
-    val scala3   = "3.3.0-RC3"
+    val scala3   = "3.3.0-RC4"
   }
 
   object Versions {
