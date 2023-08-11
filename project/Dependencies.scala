7bb0be968f82d030017a0d32e91fd807933233f3
Matthias Kurz <m.kurz@irregular.at>
2023-08-11 13:25:59 +0200

    Scala 3.3.1 / 2.13.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,8 +10,8 @@ object Dependencies {
 
   object ScalaVersions {
     val scala212 = "2.12.18"
-    val scala213 = "2.13.11"
-    val scala3   = "3.3.0"
+    val scala213 = "2.13.12"
+    val scala3   = "3.3.1"
   }
 
   object Versions {
