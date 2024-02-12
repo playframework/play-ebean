974c75031e9bf66b84a861f337d84bf54aa5cbe6
Antoine LCDP <antoine@lecomptoirdespharmacies.fr>
2024-02-12 18:00:31 +0100

    Bump version to 13.26.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
 
   object Versions {
     val play: String   = "3.0.1"
-    val ebean          = "13.23.0"
+    val ebean          = "13.26.1"
     val ebeanJakarta   = s"$ebean-jakarta"
     val typesafeConfig = "1.4.3"
   }
