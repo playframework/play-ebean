da2abfbd589dda2442f254a8b30b5a6be7717e9a
Scala Steward <scala_steward@virtuslab.com>
2024-06-06 16:29:05 +0000

    play-docs-sbt-plugin 3.0.3 (was 3.0.2)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   }
 
   object Versions {
-    val play: String   = "3.0.2"
+    val play: String   = "3.0.3"
     val ebean          = "15.1.0"
     val typesafeConfig = "1.4.3"
   }
