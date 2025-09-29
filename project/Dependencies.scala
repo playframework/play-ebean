4d7a9044be7e45f2486b1f56548a8334f053be45
Scala Steward <scala_steward@virtuslab.com>
2025-09-29 15:31:34 +0000

    play-filters-helpers, play-guice, ... 3.1.0-M3 (was 3.1.0-M2)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   }
 
   object Versions {
-    val play: String   = "3.1.0-M2"
+    val play: String   = "3.1.0-M3"
     val ebean          = "17.1.0"
     val typesafeConfig = "1.4.5"
   }
