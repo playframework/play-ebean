d6b3c0063d0e5bad2d8748ac4795a5b55c25e09a
Scala Steward <scala_steward@virtuslab.com>
2025-12-12 20:20:42 +0000

    [8.x] scala-library 2.12.21 (was 2.12.20)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ import sbt._
 object Dependencies {
 
   object ScalaVersions {
-    val scala212 = "2.12.20"
+    val scala212 = "2.12.21"
     val scala213 = "2.13.18"
     val scala3   = "3.3.7"
   }
