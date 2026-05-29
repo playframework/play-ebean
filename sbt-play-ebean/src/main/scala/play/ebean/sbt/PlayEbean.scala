6413b5a9e96395dfb454bbc99feff3f694af5768
Scala Steward <scala_steward@virtuslab.com>
2026-05-29 18:08:14 +0000

    [8.x] jaxb-runtime 4.0.9 (was 4.0.8)

--- sbt-play-ebean/src/main/scala/play/ebean/sbt/PlayEbean.scala
+++ sbt-play-ebean/src/main/scala/play/ebean/sbt/PlayEbean.scala
@@ -57,7 +57,7 @@ object PlayEbean extends AutoPlugin {
       libraryDependencies ++=
         Seq(
           "org.playframework" %% "play-ebean"   % playEbeanVersion.value,
-          "org.glassfish.jaxb" % "jaxb-runtime" % "4.0.8"
+          "org.glassfish.jaxb" % "jaxb-runtime" % "4.0.9"
         )
     )
 
