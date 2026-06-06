ca0d8eca7bd3f7420e3831fd084a2fa85ea9be93
Scala Steward <scala_steward@virtuslab.com>
2026-06-06 17:13:04 +0000

    [8.x] ebean 17.8.0 (was 17.7.0)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
 
   object Versions {
     val play: String   = "3.0.11"
-    val ebean          = "17.7.0"
+    val ebean          = "17.8.0"
     val typesafeConfig = "1.4.9"
   }
 
