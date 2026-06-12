fa982d2eb07aa511d92ff8f662ebae6129600883
Scala Steward <scala_steward@virtuslab.com>
2026-06-12 21:02:29 +0000

    [7.x] scala3-library 3.3.8 (was 3.3.7)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   object ScalaVersions {
     val scala212 = "2.12.21"
     val scala213 = "2.13.18"
-    val scala3   = "3.3.7"
+    val scala3   = "3.3.8"
   }
 
   object Versions {
